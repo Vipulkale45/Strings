3a61d5e8e680ff59535493e0811af780d30fbbe9
Vipul Kale <120079040+Vipulkale45@users.noreply.github.com>
2025-08-27 14:16:11 +0530

    Short Character From String

---
+++ ShortCharaterFromString.java
@@ -0,0 +1,13 @@
+package JAP82.String;
+
+
+import java.util.Arrays;
+
+public class ShortCharaterFromString {
+    public static void main(String[] args) {
+       String s1 = "vipul";
+       char c[]= s1.toCharArray();
+       Arrays.sort(c);
+       System.out.println(c);
+    }
+}
