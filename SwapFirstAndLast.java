0daddd55118776e18fc5fee82dd7974aa51381bb
Vipul Kale <120079040+Vipulkale45@users.noreply.github.com>
2025-08-27 14:15:01 +0530

    Swap First And Last character from string

---
+++ SwapFirstAndLast.java
@@ -0,0 +1,16 @@
+package JAP82.String;
+
+public class SwapFirstAndLast {
+    public static void main(String[] args) {
+        String s1 = "India";
+        String s2 ="";
+        s2+=s1.charAt(s1.length()-1);
+        for(int i=1;i<s1.length()-1; i++)
+        {
+            s2+=s1.charAt(i);
+        }
+        s2+=s1.charAt(0);
+        System.out.println(s2);
+
+    }
+}
