946ac602c7951639f1df633927cf8431e6a759ef
Vipul Kale <120079040+Vipulkale45@users.noreply.github.com>
2025-08-28 21:27:31 +0530

    Sort Each Word from string

---
+++ SortEachWord.java
@@ -0,0 +1,25 @@
+package JAP82.String;
+
+import java.util.Arrays;
+
+public class SortEachWord {
+    public static void main(String[] args) {
+        String s1 = "hello i am java developer";
+        String s2[]=s1.split(" ");
+        Arrays.sort(s2);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<s2.length; i++)
+        {
+             sb.append(s2[i]);
+            
+                if(i<s2.length-1)
+                {
+                    sb.append(" ");
+                }
+            
+        }
+        System.out.println(sb);
+    }
+}
+//input : hello i am java developer
+//output: am developer hello i java
